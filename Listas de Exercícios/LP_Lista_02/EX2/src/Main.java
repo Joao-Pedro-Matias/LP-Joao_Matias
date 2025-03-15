898a225b49992415c36e74b934cb5879788b5489
Joao_Pedro_Matias <joaopedromatias2007@gmail.com>
2025-03-14 22:05:23 -0300

    Lista2_EX1

---
+++ Listas de Exercícios/LP_Lista_02/EX2/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        double n1;
+        double result;
+
+        System.out.println("Digite um número:");
+        n1 = in.nextDouble();
+        result = n1*2;
+
+        System.out.println("O dobro de " + n1 + " é: " + result);
+
+    }
+}
