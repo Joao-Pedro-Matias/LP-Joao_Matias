a56447d0c9b94c3dbce52f51f9cc2dd3fce9ca46
Joao_Pedro_Matias <joaopedromatias2007@gmail.com>
2025-04-08 20:40:19 -0300

    Lista03_EX15

--- Listas de Exercícios/LP_Lista_03/EXE15/src/Main.java
+++ Listas de Exercícios/LP_Lista_03/EXE15/src/Main.java
@@ -5,16 +5,75 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
-        double money;
+        double money, change;
+        int option;
+
+        change = 0;
 
         System.out.println("Insira o dinheiro:");
         money = in.nextDouble();
 
         if (money<0.5) System.out.println("Dinheiro insuficiente");
         else {
-            if (money<=1) {
-
+            if (money<1) {
+                System.out.println("------------------------------" +
+                        "\nMENU:" +
+                        "\n1. Café Expresso R$ 0,50" +
+                        "\n ------------------------------");
             }
+            else if (money<2) {
+                System.out.println("------------------------------" +
+                        "\nMENU:" +
+                        "\n1. Café Expresso R$ 0,50" +
+                        "\n1. Café Longo R$ 1,00" +
+                        "\n ------------------------------");
+            }
+            else if (money<2.5) {
+                System.out.println("------------------------------" +
+                        "\nMENU:" +
+                        "\n1. Café Expresso R$ 0,50" +
+                        "\n1. Café Longo R$ 1,00" +
+                        "\n3. Chocolate R$ 2,00" +
+                        "\n ------------------------------");
+            }
+            else {
+                System.out.println("------------------------------" +
+                                "\nMENU:" +
+                                "\n1. Café Expresso R$ 0,50" +
+                                "\n1. Café Longo R$ 1,00" +
+                                "\n3. Chocolate R$ 2,00" +
+                                "\n4. Cappuciono R$ 2,50" +
+                        "\n ------------------------------");
+            }
+
+            System.out.println("Escolha uma opção");
+            option = in.nextInt();
+            System.out.println("------------------------------");
+
+            switch (option) {
+                case 1:
+                    System.out.println("Bebida selecionada: Café Expresso");
+                    change = money - 0.5;
+                    break;
+                case 2:
+                    System.out.println("Bebida selecionada: Café Longo");
+                    change = money - 1;
+                    break;
+                case 3:
+                    System.out.println("Bebida selecionada: Chocolate");
+                    change = money - 2;
+                    break;
+                case 4:
+                    System.out.println("Bebida selecionada: Capuccino");
+                    change = money - 2.5;
+                    break;
+                default:
+                    System.out.println("Opção Inválida");
+                    break;
+            }
+
+            System.out.println("Seu troco: " + change);
+
         }
 
     }
