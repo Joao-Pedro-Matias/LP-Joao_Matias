65599007da54bae604bed7681180a4fc10a0f83c
Joao Pedro Matias <joaopedromatias2007@gmail.com>
2025-04-15 14:06:00 -0300

    Lista04_Finish

--- Listas de Exercícios/LP_Lista_04/Ex09/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/Ex09/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -5,22 +6,45 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
-        int number, test;
+        int number, test, test2=0;
+        String start;
 
-        for (int i=1; i<=10; i++) {
-            System.out.println("-----------------------------------");
-            System.out.println("TABUADA DO " + i);
-            System.out.println("-----------------------------------");
+        for (int k=1;;k++) {
 
-            for (int j = 1; j <= 10; j++) {
-                System.out.println(i + " x " + j + " = ");
-                number = in.nextInt();
-                test = i*j;
-                if (number==test) System.out.print(" ");
-                else {
-                    i = 1000;
-                    j = 1000;
-                    System.out.println("Você Errou :(");
+            if (k==1) System.out.println("Está pronto para começar? (Y/N)");
+            else System.out.println("Quer jogar mais uma vez? (Y/N)");
+            start = in.next();
+
+
+            System.out.println("-----------------------------------");
+            if (Objects.equals(start, "Y") || Objects.equals(start, "y")) {
+                test2 = 0;
+                System.out.println("JOGO DA TABUADA");
+            }
+            else {
+                System.out.println("Finalizando");
+                System.out.println("-----------------------------------");
+                break;
+            }
+
+
+            for (int i = 1; i <= 10; i++) {
+                if (test2 == 1) break;
+                System.out.println("-----------------------------------");
+                System.out.println("TABUADA DO " + i);
+                System.out.println("-----------------------------------");
+
+                for (int j = 1; j <= 10; j++) {
+                    System.out.println(i + " x " + j + " = ");
+                    number = in.nextInt();
+                    test = i * j;
+                    if (number == test) continue;
+                    else {
+                        test2 = 1;
+                        System.out.println("Você Errou :(");
+                        System.out.println("-----------------------------------");
+                        break;
+                    }
                 }
             }
         }
--- Listas de Exercícios/LP_Lista_04/Ex10/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/Ex10/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner in = new Scanner(System.in);
+
+        int n, number, lastNumber, numberPrint;
+        number = 1;
+        lastNumber = 0;
+        numberPrint = 0;
+
+        System.out.println("Até quantos numeros você quer ver a sequência?");
+        n = in.nextInt();
+
+        for (int i=0; i<n; i++) {
+
+            if (i == 0 ){
+                System.out.print("0, ");
+                continue;
+            }
+
+            lastNumber = number;
+            number = numberPrint;
+            numberPrint = number + lastNumber;
+            System.out.print(numberPrint + ", ");
+
+        }
     }
 }
