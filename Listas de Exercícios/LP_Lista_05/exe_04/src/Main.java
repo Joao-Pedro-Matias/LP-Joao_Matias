1aa8597e1bd8f77943c0f73975f795731661f498
Joao_Pedro_Matias <joaopedromatias2007@gmail.com>
2025-04-15 22:03:13 -0300

    Lista05_EX04

--- Listas de Exercícios/LP_Lista_05/exe_04/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/exe_04/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner in = new Scanner(System.in);
+
+        int [] numbers = new int[10];
+
+        int bigger=0, pointerBigger=0;
+
+        System.out.println("Digite 10 números separados por espaço");
+
+        for (int i=0; i<numbers.length; i++) {
+            System.out.println("Digite o " + (i+1) + "° número");
+            numbers[i] = in.nextInt();
+        }
+        for (int i=0; i<numbers.length; i++ ) {
+            if (numbers[i]>bigger) {
+                bigger = numbers[i];
+                pointerBigger = i;
+            }
+        }
+
+        System.out.println("O maior número é " + bigger + " e foi encontrado na posição " + (pointerBigger+1));
+
     }
 }
