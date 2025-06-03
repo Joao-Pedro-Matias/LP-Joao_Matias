55528c2ca3783a46844d851b1e8d9364693c561c
Joao_Pedro_Matias <joaopedromatias2007@gmail.com>
2025-06-02 22:54:58 -0300

    Lista_06-EX05-NoFinish

--- Listas de Exercícios/LP_Lista_06/ex05/src/Main.java
+++ Listas de Exercícios/LP_Lista_06/ex05/src/Main.java
@@ -5,21 +5,32 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
+
         int [] numbers = {13, 15, 24, 30, 33, 40, 42, 52, 69, 71, 88};
         int n;
 
         System.out.println("Digite o número a ser buscado: ");
         n = in.nextInt();
 
-        int result = findNumber(numbers, n);
+        int result = findNumber(numbers, n, numbers.length,0);
+
+        System.out.println(result);
+        System.out.println(numbers[result]);
 
     }
 
-    public static int findNumber(int [] numbers, int n) {
-        int max = 0;
-        int min = 0;
-        if (n>numbers[numbers.length/2]) {
+    public static int findNumber(int [] numbers, int n, int len, int aux) {
 
+        if (n == numbers[len/2+aux]) return len/2+aux;
+        if (n>numbers[len/2+aux] && n<numbers[len/2+aux]) return -1;
+        else {
+            if (n > numbers[len/2+aux]) {
+                aux += len/2;
+                len /= 2;
+            }
+            else
+                len /= 2;
+            return findNumber(numbers, n,len,aux);
         }
     }
 }
